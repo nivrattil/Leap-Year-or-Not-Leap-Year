dcc9e94f9747697da0a00bb698a3008d93577505
Nivratti Waghmode <94159696+nivrattil@users.noreply.github.com>
2023-02-28 00:11:57 +0530

    Add files via upload

---
+++ Leap Year or Not in cpp.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int year;
+    cout<<"Enter Year:";
+    cin>>year;
+    if(year%4==0)
+    {
+        if(year%100==0){
+            if(year%400==0){
+                cout<<"Leap Year";
+            }
+            else
+            {
+                cout<<"Not Leap Year";
+            }
+         
+        }
+        else
+            {
+                cout<< "Leap Year";
+            }
+    }
+    else
+            {
+                cout<< "Not Leap Year";
+            }
+            return 0;
+}
+
